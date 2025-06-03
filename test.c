d506774e1f9d54b228669ed25411e8b147a0e5cc
Your Name <you@example.com>
2025-06-02 18:28:46 -0700

    Fixed name

--- test.c
+++ test.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <string.h>
 
-void draw_weather_app(uint32_t *buf) {
-    // Weather app interface
+void draw_test_app(uint32_t *buf) {
     draw_text_centered(buf, "TEST", MEDIUM_TEXT, STATUS_HEIGHT + 150, COLOR_WHITE);
 }
